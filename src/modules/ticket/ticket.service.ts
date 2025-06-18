03104f49f371ecd6b51e232230baa8b1a42e814d
Coco <coulet.corentin@gmail.com>
2025-06-18 18:49:00 +0200

    commit

--- src/modules/ticket/ticket.service.ts
+++ src/modules/ticket/ticket.service.ts
@@ -1,9 +1,10 @@
 import prisma from '@/lib/prisma';
+import { Prisma } from '@prisma/client';
 
 export async function list() {
   return prisma.ticket.findMany();
 }
 
-export async function create(data: any) {
+export async function create(data: Prisma.TicketCreateInput) {
   return prisma.ticket.create({ data });
 }
